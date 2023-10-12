e9b173dcdb5fffe51e9f52577f80b7e7fd152deb
osminn <osm1525@naver.com>
2023-10-12 17:42:03 +0900

    오상민 공지사항, 일정
    10/12

--- levelup/src/main/java/com/gs/levelup/notice/model/service/NoticeServiceImpl.java
+++ levelup/src/main/java/com/gs/levelup/notice/model/service/NoticeServiceImpl.java
@@ -1,5 +1,71 @@
 package com.gs.levelup.notice.model.service;
 
-public class NoticeServiceImpl {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gs.levelup.common.Paging;
+import com.gs.levelup.common.Search;
+import com.gs.levelup.common.SearchDate;
+import com.gs.levelup.notice.model.dao.NoticeDao;
+import com.gs.levelup.notice.model.vo.Notice;
+
+@Service("noticeService")
+public class NoticeServiceImpl implements NoticeService{
+	@Autowired	//자동 DI 처리됨 : 자동 객체 생성됨
+	private NoticeDao noticeDao;
+	
+	@Override
+	public ArrayList<Notice> selectList(Paging paging){
+		return noticeDao.selectList(paging);
+	}
+	@Override
+	public Notice selectOne(int noticeNo) {
+		return noticeDao.selectOne(noticeNo);
+	}
+	@Override
+	public int insertNotice(Notice notice) {
+		return noticeDao.insertNotice(notice);
+	}
+	@Override
+	public int updateNotice(Notice notice) {
+		return noticeDao.updateNotice(notice);
+	}
+	@Override
+	public int deleteNotice(int noticeNo) {
+		return noticeDao.deleteNotice(noticeNo);
+	}
+	@Override
+	public int selectListCount() {
+		return noticeDao.selectListCount();
+	}
+	@Override
+	public int selectSearchTitleCount(String keyword) {
+		return noticeDao.selectSearchTitleCount(keyword);
+	}
+	@Override
+	public int selectSearchContentCount(String keyword) {
+		return noticeDao.selectSearchContentCount(keyword);
+	}
+	@Override
+	public int selectSearchDateCount(SearchDate date) {
+		return noticeDao.selectSearchDateCount(date);
+	}
+	@Override
+	public ArrayList<Notice> selectSearchTitle(Search search) {
+		return noticeDao.selectSearchTitle(search);
+	}
+	@Override
+	public ArrayList<Notice> selectSearchContent(Search search) {
+		return noticeDao.selectSearchContent(search);
+	}
+	@Override
+	public ArrayList<Notice> selectSearchDate(Search search) {
+		return noticeDao.selectSearchDate(search);
+	}
+	@Override
+	public void updateReadCount(int noticeNo) {
+	
+	}
 }
