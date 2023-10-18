d296149397605bf8bf1c6e0f787d232f431aac91
kimjihyuk00 <kimjihyuk_@naver.com>
2023-10-18 19:06:11 +0900

    no message

--- levelup/src/main/java/com/gs/levelup/item/model/dao/ItemDao.java
+++ levelup/src/main/java/com/gs/levelup/item/model/dao/ItemDao.java
@@ -2,12 +2,17 @@ package com.gs.levelup.item.model.dao;
 
 import java.util.ArrayList;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.gs.levelup.item.model.vo.Item;
 
 @Repository("itemDao")
 public class ItemDao {
+	
+	@Autowired	//root-context.xml 에서 생성한 객체를 자동 연결함
+	private SqlSessionTemplate sqlSessionTemplate;
 
 	public ArrayList<Item> selectListItems() {
 		// TODO Auto-generated method stub
@@ -27,6 +32,10 @@ public class ItemDao {
 	public ArrayList<Item> selectSearchItems(String searchword) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Item selectOneItem(String itemName) {
+		return sqlSessionTemplate.selectOne("itemMapper.selectOneItem", itemName);
 	}
 
 }
